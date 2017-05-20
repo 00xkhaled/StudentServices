efdf1e72a045851a6f50de13a8c61174c956729b
Ahmad Tamimi <ahmad.y.tamimi@gmail.com>
2017-05-20 17:53:35 +0300

    no message

--- src/java/beans/MapBean.java
+++ src/java/beans/MapBean.java
@@ -1,14 +1,21 @@
 package beans;
 
 import java.io.Serializable;
+import javax.annotation.ManagedBean;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.view.ViewScoped;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
-
+@ManagedBean
 @SessionScoped
+    
+
 public class MapBean implements Serializable {
+    private MapModel mapModel;    
+    private double lat;  
+    private double lng;  
 private MapModel model = new DefaultMapModel();
 public MapBean() {
 model.addOverlay(new Marker(new LatLng(31.777067, 35.802555), "GJU"));
