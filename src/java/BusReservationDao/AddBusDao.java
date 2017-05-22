b0e3fb7e9ca57369d8695b293f2d0988e3101d00
Kamal Jabari <emperorkamal@gmail.com>
2017-05-22 18:23:49 +0300

    Busreservation:  ADD Busreservation/admin backend packages

--- src/java/BusReservationDao/AddBusDao.java
+++ src/java/BusReservationDao/AddBusDao.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package busReservationBean;
+package BusReservationDao;
 
 /**
  *
  * @author Kamal Jabari
  */
-public class AddDriverBeans {
+public class AddBusDao {
     
 }
--- src/java/BusReservationModel/AddDriver.java
+++ src/java/BusReservationModel/AddDriver.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package busReservationBean;
+package BusReservationModel;
 
 /**
  *
  * @author Kamal Jabari
  */
-public class AddStudentBean {
+public class AddDriver {
     
 }
