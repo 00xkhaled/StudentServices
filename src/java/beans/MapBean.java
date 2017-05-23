dce1f4310a117586ae1b98387b33ed6e395cb489
Ahmad Tamimi <ahmad.y.tamimi@gmail.com>
2017-05-23 13:02:35 +0300

    no message

--- src/java/beans/MapBean.java
+++ src/java/beans/MapBean.java
@@ -4,11 +4,12 @@ import java.io.Serializable;
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
-@ManagedBean
+@Named("mapBean")
 @SessionScoped
     
 
@@ -16,12 +17,18 @@ public class MapBean implements Serializable {
     private MapModel mapModel;    
     private double lat;  
     private double lng;  
+    double x=31.777067;
+    double y=35.802555;
 private MapModel model = new DefaultMapModel();
 public MapBean() {
-model.addOverlay(new Marker(new LatLng(31.777067, 35.802555), "GJU","http://maps.google.com/mapfiles/ms/micons/pink-dot.png"));
+model.addOverlay(new Marker(new LatLng(x,y), "GJU"));
 }
 public MapModel getModel() {
 return this.model;
 }
+public void update(){
+x=x+0.01;    
+    model.addOverlay(new Marker(new LatLng(x, y), "GJU"));
 
+}
 }
