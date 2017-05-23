fb651cfe0354bdfa50fe521fc5ae6a1e8e6a30cd
Ahmad Tamimi <ahmad.y.tamimi@gmail.com>
2017-05-23 11:20:21 +0300

    Revert "gmap+mapbean"
    This reverts commit 07ae77d937c3470158c10194358e8ab0ad2410ea.

--- src/java/beans/MapBean.java
+++ src/java/beans/MapBean.java
@@ -3,31 +3,24 @@ package beans;
 import java.io.Serializable;
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
+import javax.faces.view.ViewScoped;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
-
-
-@Named("mapBean")
+@ManagedBean
 @SessionScoped
+    
 
-
-public class MapBean implements Serializable { 
-double x=31.777067;
-double y=35.802555;
-LatLng center;
-MapModel model = new DefaultMapModel();  
-public MapBean(){
-model.addOverlay(new Marker(new LatLng(x,y), "GJU"));
+public class MapBean implements Serializable {
+    private MapModel mapModel;    
+    private double lat;  
+    private double lng;  
+private MapModel model = new DefaultMapModel();
+public MapBean() {
+model.addOverlay(new Marker(new LatLng(31.777067, 35.802555), "GJU","http://maps.google.com/mapfiles/ms/micons/pink-dot.png"));
 }
 public MapModel getModel() {
 return this.model;
-}
-public void update(){
-    x=x+0.01;
-    model.addOverlay(new Marker(new LatLng(x,y+0.00001), "GJU"));
-    center = new LatLng(x,y);
 }
 }
