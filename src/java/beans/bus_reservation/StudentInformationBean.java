36ce36212046649208eda292b14014b7c5420407
Kamal Jabari <emperorkamal@gmail.com>
2017-05-31 15:46:33 +0300

    SessionScoped  to ViewScoped

--- src/java/beans/bus_reservation/StudentInformationBean.java
+++ src/java/beans/bus_reservation/StudentInformationBean.java
@@ -1,7 +1,6 @@
 package beans.bus_reservation;
 
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import javax.inject.Inject;
 import daos.bus_reservation.StudentInformationDao;
@@ -11,81 +10,86 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import models.bus_reservation.StudentInformation;
 import beans.SessionBean;
+import javax.faces.view.ViewScoped;
+
 /**
  *
  * @author Kamal Jabari
  */
-@Named(value = "StudentInformationBean")
-@SessionScoped
+@Named(value = "studentInformationBean")
+@ViewScoped
 public class StudentInformationBean implements Serializable {
-    
-    
-    
+
     private String student_id;
     private String student_name;
     private String phone;
     private String seat_pre_res;
-    
-    
+
     private StudentInformation selectedStudent;//model object to save selected student;
-    private final StudentInformationDao std_inf_dao=new StudentInformationDao();//dao object;
+    private final StudentInformationDao std_inf_dao = new StudentInformationDao();//dao object;
     private ArrayList<StudentInformation> list;//list of type model to save result from database
-    
-     @Inject
+
+    @Inject
     private SessionBean sessionBean;
-     
-    public StudentInformationBean(){
-        init();
-    
+
+    public StudentInformationBean() {
+
     }
-    
-     @PostConstruct
-    public void init(){
-        try {            
-            list = std_inf_dao.buildStudent();            
+
+    @PostConstruct
+    public void init() {
+        try {
+            list = std_inf_dao.buildStudent();
         } catch (Exception ex) {
             Logger.getLogger(StudentInformationBean.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public String getStudentID(){
+
+    public String getStudentID() {
         return this.student_id;
     }
-    public void setStudentID(String student_id){
-        this.student_id=student_id;
+
+    public void setStudentID(String student_id) {
+        this.student_id = student_id;
     }
-     public StudentInformation getSelectedStudent() {//selected model 
+
+    public StudentInformation getSelectedStudent() {//selected model 
         return selectedStudent;
     }
+
     public void setSelectedStudent(StudentInformation selectedStudent) {
         this.selectedStudent = selectedStudent;
-    } 
+    }
+
     public ArrayList<StudentInformation> getStudent() {
         return list;
     }
-     public String getStudentName(){
+
+    public String getStudentName() {
         return this.student_name;
     }
-    public void setStudentName(String student_name){
-        this.student_name=student_name;
+
+    public void setStudentName(String student_name) {
+        this.student_name = student_name;
     }
-    
-     public String getPhone(){
+
+    public String getPhone() {
         return this.phone;
     }
-    public void setPhone(String student_phone){
-        this.phone=student_phone;
+
+    public void setPhone(String student_phone) {
+        this.phone = student_phone;
     }
-    
-    
-    
-     public String getSeatPreRes(){
+
+    public String getSeatPreRes() {
         return this.seat_pre_res;
     }
-    public void setSeatPreRes(String seat_pre_res){
-        this.seat_pre_res=seat_pre_res;
+
+    public void setSeatPreRes(String seat_pre_res) {
+        this.seat_pre_res = seat_pre_res;
     }
-    
-    public void saveSelectedStudentID(){
+
+    public void saveSelectedStudentID() {
         sessionBean.setSelectedItemId(selectedStudent.getStudentID());
     }
 
@@ -96,6 +100,5 @@ public class StudentInformationBean implements Serializable {
     public void setList(ArrayList<StudentInformation> list) { //TO SET IN THE list of type model to save result from database
         this.list = list;
     }
-    
-    
+
 }
