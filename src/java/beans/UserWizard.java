5d1e98131c8fb615961d014a39f9d8a743c28d9d
Khaled Daoudieh <Khaled@EvilOpeth.lan>
2017-06-01 03:03:12 +0300

    added the Wizard page bean that includes handler for the calendar

---
+++ src/java/beans/UserWizard.java
@@ -0,0 +1,73 @@
+package beans;
+
+
+/**
+ *
+ * @author Khaled
+ */
+import java.text.SimpleDateFormat;
+ 
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.FlowEvent;
+import models.RequestInformation;
+ 
+@ManagedBean
+@ViewScoped
+
+public class UserWizard implements Serializable {
+    
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+     
+    public void click() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+         
+        requestContext.update("form:display");
+        requestContext.execute("PF('dlg').show()");
+    }
+ 
+    private RequestInformation user = new RequestInformation();
+     
+    private boolean skip;
+     
+    public RequestInformation getUser() {
+        return user;
+    }
+ 
+    public void setUser(RequestInformation user) {
+        this.user = user;
+    }
+     
+    public void save() {        
+        FacesMessage msg = new FacesMessage("Successful", "Welcome :" + user.getFirstname());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+     
+    public boolean isSkip() {
+        return skip;
+    }
+ 
+    public void setSkip(boolean skip) {
+        this.skip = skip;
+    }
+     
+    public String onFlowProcess(FlowEvent event) {
+        if(skip) {
+            skip = false;   //reset in case user goes back
+            return "confirm";
+        }
+        else {
+            return event.getNewStep();
+        }
+    }
+}
